e1abc0cfbf1ee890fe2ced28be5d926fec99e32b
leedaye0412 <neoldy00@naver.com>
2024-03-26 17:46:26 +0900

    Update dataframe.py secomd

--- dataframe.py
+++ dataframe.py
@@ -20,18 +20,20 @@ def plot_matplotlib(): #streamlit 문법
     # Labeling axes and title
     ax.set_xlabel("year")
     ax.set_ylabel("lifeExp")
-    ax.set_title("Categorical Bar Plot")
+    ax.set_title("'Year' vs 'lifeExp'")
     
     st.pyplot(fig) # 대시보드에 출력
 
 def main():
     st.title("Data Display st.dataframe()")
     st.checkbox("Use container width", value=False, key = 'use_container_width')
+
     
     df = load_data()
     st.dataframe(df, use_container_width=True) # 대시보드에 표 삽입
 
     #pandas style- 두번째 표
+    st.title("Maximum value per colum")
     st.dataframe(df.iloc[:5,2:].style.highlight_max(axis=0))
 
     plot_matplotlib() #그래프 삽입
